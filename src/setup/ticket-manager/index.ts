7f352983d36cf2325246d047745b51fa6cb3263d
Shawn Ifill <shawn.ifill@gmail.com>
2024-12-11 13:38:01 -0500

    SHARD-1097: Set minimum signatures for ticekts signature to 2

--- src/setup/ticket-manager/index.ts
+++ src/setup/ticket-manager/index.ts
@@ -72,7 +72,7 @@ export async function updateTicketMap(): Promise<void> {
     const ticketTypes: TicketType[] = await getTicketTypesFromArchiver(archiver)
 
     const devPublicKeys = shardusConfig?.debug?.multisigKeys || {}
-    const requiredSigs = Math.max(1, shardusConfig?.debug?.minMultiSigRequiredForGlobalTxs || 3)
+    const requiredSigs = Math.max(2, shardusConfig?.debug?.minMultiSigRequiredForGlobalTxs || 1)
 
     ticketTypes.forEach((ticketType: TicketType, i: number) => {
       const { sign, ...ticketTypeWithoutSign } = ticketType
